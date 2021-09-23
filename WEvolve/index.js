1f62437c5ba09ce129f75fc2bc89dd1cd52ed033
Paras Jain <55691273+parasjain2792@users.noreply.github.com>
2021-09-23 16:46:30 +0530

    Update index.js

--- WEvolve/index.js
+++ WEvolve/index.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGOURL).then(()=>{
 
     if (process.env.NODE_ENV === 'production') {
         // Set static folder
-        app.use(express.static('client/build'));
+        app.use(express.static('Client/build'));
     
         app.get('*', (req, res) => {
             res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
